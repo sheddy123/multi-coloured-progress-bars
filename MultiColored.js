fb7a407efc8cba3cd16ded5998b003b68dd2fc5d
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2022-02-26 15:28:32 +0100

    Commit staged

--- MultiColored.js
+++ MultiColored.js
@@ -2,18 +2,25 @@ var i = 0;
 function move() {
   if (i == 0) {
     i = 1;
+
+    //Get each progressBar element
     var elem = document.getElementById('myBar');
     var elem1 = document.getElementById('myBar1');
     var elem2 = document.getElementById('myBar2');
     var elem3 = document.getElementById('myBar3');
     var elem4 = document.getElementById('myBar4');
+
+    //Set the width of the progressBar to 50
     var width = 50;
+
+    //Set the width of the time interval for the progressbar to increase by 10
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
         i = 0;
       } else {
+        //Increment width
         width++;
         elem.style.width = width + '%';
         elem.innerHTML = width + '%';
