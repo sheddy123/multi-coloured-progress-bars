8cb1dff1aea9366d3b48a5da24a74a9cec4a5cb8
Ifeanyi Shadrach Odom <ifeanyishadrach452@gmail.com>
2022-02-26 15:17:27 +0100

    Add functionality for other progressbars

--- MultiColored.js
+++ MultiColored.js
@@ -3,7 +3,11 @@ function move() {
   if (i == 0) {
     i = 1;
     var elem = document.getElementById('myBar');
-    var width = 10;
+    var elem1 = document.getElementById('myBar1');
+    var elem2 = document.getElementById('myBar2');
+    var elem3 = document.getElementById('myBar3');
+    var elem4 = document.getElementById('myBar4');
+    var width = 50;
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
@@ -13,6 +17,18 @@ function move() {
         width++;
         elem.style.width = width + '%';
         elem.innerHTML = width + '%';
+
+        elem1.style.width = width + '%';
+        elem1.innerHTML = width + '%';
+
+        elem2.style.width = width + '%';
+        elem2.innerHTML = width + '%';
+
+        elem3.style.width = width + '%';
+        elem3.innerHTML = width + '%';
+
+        elem4.style.width = width + '%';
+        elem4.innerHTML = width + '%';
       }
     }
   }
